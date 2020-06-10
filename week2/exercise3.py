b2fdcce7e14a01925280d55bf29930df9e15ea0b
john chen <jaqc2008@hotmail.com>
2020-06-11 00:05:03 +1000

    Exercise2 completed

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -7,10 +7,7 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    if a_number % 2 == 0:
-        ans = False
-    else:
-        ans = True
+    ans = a_number % 2 == 1
     return ans
 
 
@@ -23,14 +20,18 @@ def fix_it(moves=True, should_move=True):
     Use conditional statements: if, else, elif etc.
     This function should return either:
     "WD-40"
-    "Duct Tape"
+    "Duct Tape"         
     "No Problem"
 
     Most people write this function with 4 return statements. 
     As an extra challenge, see if you can get that down to three.
     """
-    return None
-
+    if (moves == False and should_move == False):
+        return "WD-40"
+    elif (moves == True and should_move == False):
+        return "Duct Tape"
+    else:
+        return "No Problem"
 
 def loops_1a():
     """Make 10 stars.
@@ -39,7 +40,11 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    star ='*'
+    for shine in star:
+        ans = list(range(10))
+        print (star)
+    return ans
 
 
 def loops_1c(number_of_items=5, symbol="#"):
